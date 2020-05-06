58c6e761d004e315ab04402ebe748dcf2ddfd99c
Itunuoluwao <itunuoluwa.olurin@gmail.com>
2020-05-06 03:06:53 +0100

    Updates

---
+++ precache-manifest.289fd60759b16d75a1ef5b55b59988c9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "582376483faddea64d177db0ba7298e7",
+    "url": "/scamp-project/index.html"
+  },
+  {
+    "revision": "a7421dfb7f9ac2b8fcd9",
+    "url": "/scamp-project/static/css/main.e8bc336c.chunk.css"
+  },
+  {
+    "revision": "8dd0ca21b37120b186f3",
+    "url": "/scamp-project/static/js/2.e1075850.chunk.js"
+  },
+  {
+    "revision": "176852ae27885dfd7559e0fb4b332d24",
+    "url": "/scamp-project/static/js/2.e1075850.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a7421dfb7f9ac2b8fcd9",
+    "url": "/scamp-project/static/js/main.9bb2aaaf.chunk.js"
+  },
+  {
+    "revision": "f1a5a64b2a35efc89210",
+    "url": "/scamp-project/static/js/runtime-main.3f228662.js"
+  }
+]);
